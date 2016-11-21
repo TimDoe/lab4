0bad77761d0275e1ca01f7b054dd3ced5d3bf1c9
Matthias <Matthias@Matthias-PC>
2016-11-21 19:01:33 +0100

    Created Edge.java

--- src/de/ws1617/pccl/fsa/Edge.java
+++ src/de/ws1617/pccl/fsa/Edge.java
@@ -1,10 +1,35 @@
 package de.ws1617.pccl.fsa;
 
+import de.ws1617.pccl.grammar.Terminal;
+
 /**
  * A directed edge (transition) between states.
  *
  */
-public class Edge {
 
-	// TODO implement me !
+public class Edge 
+{
+	Terminal toConsume;
+	int goal;
+	public Edge(Terminal aTerminal, int aGoal) 
+	{
+		toConsume = aTerminal;
+		goal = aGoal;
+
+
+	}
+	public int change(Terminal aTerminal)
+	{
+		if (toConsume.equals(aTerminal))
+		{
+			return goal;
+		}
+		else return -1;
+
+
+	}
+
+
+
 }
+
